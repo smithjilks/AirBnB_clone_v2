93b367d240f81020534f4196de385e0ecef9d26a
smithjilks <smithjilks@gmail.com>
2021-12-22 18:18:28 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """getter method
+        Returns:
+            list: returns the list of City instances with state_id equals
+                  to the current State.id
+        """
+        cities_list = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                cities_list.append(city)
+        return cities_list
