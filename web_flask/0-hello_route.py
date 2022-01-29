ad79cd5018ec19221b12ddf4ccbbf20413a4eb92
smithjilks <smithjilks@gmail.com>
2022-01-29 14:35:18 +0300

    Add 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Starts a Flask web application listeniing on 0.0.0.0:5000"""
+
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
