2a1765cd0d459ccda9d83d402ef4e83568c1c3b0
smithjilks <smithjilks@gmail.com>
2022-01-30 23:34:14 +0300

    Update db-storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,7 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """ method for deserializing the JSON file to objects"""
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -10,19 +10,27 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        """getter method
-        Returns:
+    def __init__(self, *args, **kwargs):
+        """intialize state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """getter method
+            Returns:
             list: list of City instances with state_id equals
                   to the current State.id
-        """
-        cities_list = []
-        for city in models.storage.all(City).values():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+            """
+            cities_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
